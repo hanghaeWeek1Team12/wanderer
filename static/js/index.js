6b0766d3e9e183497735efb16d66700219eec8fb
dohyung97022 <dohyung97022@gmail.com>
2021-06-08 13:42:19 +0900

    css 배경 고정 연구

--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,35 @@
-function login() {
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/login",
-        data: {
-            email: "dohyung97022@tester.com",
-            password: "9999",
-            nickname: "kimdoe"
-        },
-        success:
-            function (response) {
-                console.log(response)
-                const jwt = response['val']
-                placelist(jwt)
-            }
-    })
-}
+// function login() {
+//     $.ajax({
+//         type: "POST",
+//         url: "http://localhost:8080/login",
+//         data: {
+//             email: "dohyung97022@tester.com",
+//             password: "9999",
+//             nickname: "kimdoe"
+//         },
+//         success:
+//             function (response) {
+//                 console.log(response)
+//                 const jwt = response['val']
+//                 placelist(jwt)
+//             }
+//     })
+// }
 
-function placelist(jwt) {
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/placelist",
-        data: {
-            jwt: jwt
-        },
-        success:
-            function (response) {
-                console.log(response)
-            }
-    })
-}
+// function placelist(jwt) {
+//     $.ajax({
+//         type: "POST",
+//         url: "http://localhost:8080/placelist",
+//         data: {
+//             jwt: jwt
+//         },
+//         success:
+//             function (response) {
+//                 console.log(response)
+//             }
+//     })
+// }
 
-login()
 
 
 
