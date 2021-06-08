09552f2119ffcf80914849dc40727512f67830d2
Song Yoon <ysong0504@gmail.com>
2021-06-08 19:06:14 +0900

    main template 수정'

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,17 @@
-console.log("테스팅!")
+// 좋아요 기능
+function toggle_like(placeName,status) {
+    let email = "ysong0504@gmail.com"
+    $.ajax({
+        type: "POST",
+        url: "/like",
+        data: {
+            placeName_give: placeName,
+            email_give: email,
+            status_give: status
+        },
+        success: function (response) {
+            window.location.reload()
+        }
+    })
+
+}
